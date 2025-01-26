afc1026228fc3d5ca75ff2d9fefd2a5f324e002f
Nestor Otondo <oto23001@byui.edu>
2025-01-26 20:47:29 -0300

    Removing unnecessary requires

--- routers/ticketRouter.js
+++ routers/ticketRouter.js
@@ -1,6 +1,5 @@
 const ticketRouter = require('express').Router();
 const ticketController = require('../controllers/ticketController');
-require('./ticketSwagger');
 
 ticketRouter.get('/', ticketController.getAllTickets);
 ticketRouter.get('/:id', ticketController.getTicketById);
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,5 @@
 const userRouter = require('express').Router();
 const userController = require('../controllers/userController');
-require('./userSwagger');
 
 userRouter.get('/', userController.getAllUsers);
 userRouter.get('/:id', userController.getUserById);
